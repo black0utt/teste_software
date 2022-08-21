d0c96c612024ae5b1aa92a8e4022e4e43c789237
Wesley Santos <wesleysv61@gmail.com>
2022-08-21 12:50:26 -0300

    backup antes de instalar o fetch

--- tests/teste.test.tsx
+++ tests/teste.test.tsx
@@ -119,93 +119,191 @@ const puppeteer = require('puppeteer');
 
 //===================================================================//
 
-describe('Percorrendo as rotas', () =>{
-  //espera-se que os testes falhe caso não contenha o /numero especificado
-  beforeAll(async () => {
+// describe('Percorrendo as rotas', () =>{
+//   //espera-se que os testes falhem caso não contenha o /numero especificado ou o texto referente a cada página
+//   beforeAll(async () => {
     
+//     const browser = await puppeteer.launch();
+//     return browser
+//   })
+//   test('validando a primeira rota, mega sena.', async () =>{
+//     const page = await browser.newPage();
+//     await page.goto('http://localhost:3000/0');
+//     await expect(page.url()).toContain('/0')
+
+//     await page.waitForSelector('div.logo > span')
+//     let element = await page.$('div.logo > span')   
+//     let value = await page.evaluate(el => el?.textContent, element)
+//     //o teste irá falhar caso o valor seja diferente de  quina
+//     expect(value).toContain('mega-sena')
+//     await page.close();
+//   });
+//   test('validando a segunda rota, quina.', async () =>{
+//     const page = await browser.newPage();
+//     await page.goto('http://localhost:3000/1');
+
+//     await expect(page.url()).toContain('/1')
+//     await page.waitForSelector('div.logo > span')
+//     let element = await page.$('div.logo > span')   
+//     let value = await page.evaluate(el => el?.textContent, element)
+//     //o teste irá falhar caso o valor seja diferente de  quina
+//     expect(value).toContain('quina')
+//     await page.close();
+//   });
+//   test('validando a terceira rota, LOTOFÁCIL.', async () =>{
+//     const page = await browser.newPage();
+//     await page.goto('http://localhost:3000/2');
+//     await expect(page.url()).toContain('/2')
+
+//     await page.waitForSelector('div.logo > span')
+//     let element = await page.$('div.logo > span')   
+//     let value = await page.evaluate(el => el?.textContent, element)
+//     //o teste irá falhar caso o valor seja diferente de lotofácil
+//     expect(value).toContain('lotofácil')
+//     await page.close();
+
+//   });
+//   test('validando a quarta rota, LOTOMANIA.', async () =>{
+//     const page = await browser.newPage();
+//     await page.goto('http://localhost:3000/3');
+//     await expect(page.url()).toContain('/3')
+
+//     await page.waitForSelector('div.logo > span')
+//     let element = await page.$('div.logo > span')   
+//     let value = await page.evaluate(el => el?.textContent, element)
+//     //o teste irá falhar caso o valor seja diferente de lotomania
+//     expect(value).toContain('lotomania')
+//     await page.close();
+
+
+//   });
+//   test('validando a quinta rota, TIMEMANIA.', async () =>{
+//     const page = await browser.newPage();
+//     await page.goto('http://localhost:3000/4');
+//     await expect(page.url()).toContain('/4')
+
+//     await page.waitForSelector('div.logo > span')
+//     let element = await page.$('div.logo > span')   
+//     let value = await page.evaluate(el => el?.textContent, element)
+//     //o teste irá falhar caso o valor seja diferente de timemania
+//     expect(value).toContain('timemania')
+//     await page.close();
+
+
+//   });
+//   test('validando a sexta rota, DIA DE SORTE.', async () =>{
+//     const page = await browser.newPage();
+//     await page.goto('http://localhost:3000/5');
+//     await expect(page.url()).toContain('/5')
+
+//     await page.waitForSelector('div.logo > span')
+//     let element = await page.$('div.logo > span')   
+//     let value = await page.evaluate(el => el?.textContent, element)
+//     //o teste irá falhar caso o valor seja diferente de dia de sorte
+//     expect(value).toContain('dia de sorte')
+//     await page.close();
+
+
+//   });
+  
+// })
+
+
+describe('Validando números', () =>{
+  beforeAll(async () => {
+    jest.setTimeout(30000)
     const browser = await puppeteer.launch();
     return browser
   })
-  test('validando a primeira rota, mega sena.', async () =>{
+  test('validando os números da primeira rota, mega sena.', async () =>{
     const page = await browser.newPage();
     await page.goto('http://localhost:3000/0');
-    await expect(page.url()).toContain('/0')
 
-    await page.waitForSelector('div.logo > span')
-    let element = await page.$('div.logo > span')   
-    let value = await page.evaluate(el => el?.textContent, element)
-    //o teste irá falhar caso o valor seja diferente de  quina
-    expect(value).toContain('mega-sena')
+    await page.waitForSelector('div.ball')
+    let element = await page.$$('div.ball')   
+    // let value = await page.evaluate(el => el[0]?.innerText, element)
+    let values = await page.$$eval('div.ball', elems => elems.map(elem => elem.textContent))
+    // let element = await page.$$('div.ball')   
+    // let value = await page.evaluate(el => el[0]?.innerText, element)
+
+    let pegandoNumeros = fetch("https://brainn-api-loterias.herokuapp.com/api/v1/concursos/2359").then(function(data) {
+        return data
+    });
+    
+    console.log(values)
+    console.log(pegandoNumeros)
+    
+    expect(values).toContain('a')
     await page.close();
   });
-  test('validando a segunda rota, quina.', async () =>{
-    const page = await browser.newPage();
-    await page.goto('http://localhost:3000/1');
+  // test('validando a segunda rota, quina.', async () =>{
+  //   const page = await browser.newPage();
+  //   await page.goto('http://localhost:3000/1');
 
-    await expect(page.url()).toContain('/1')
-    await page.waitForSelector('div.logo > span')
-    let element = await page.$('div.logo > span')   
-    let value = await page.evaluate(el => el?.textContent, element)
-    //o teste irá falhar caso o valor seja diferente de  quina
-    expect(value).toContain('quina')
-    await page.close();
-  });
-  test('validando a terceira rota, LOTOFÁCIL.', async () =>{
-    const page = await browser.newPage();
-    await page.goto('http://localhost:3000/2');
-    await expect(page.url()).toContain('/2')
+  //   await expect(page.url()).toContain('/1')
+  //   await page.waitForSelector('div.logo > span')
+  //   let element = await page.$('div.logo > span')   
+  //   let value = await page.evaluate(el => el?.textContent, element)
+  //   //o teste irá falhar caso o valor seja diferente de  quina
+  //   expect(value).toContain('quina')
+  //   await page.close();
+  // });
+  // test('validando a terceira rota, LOTOFÁCIL.', async () =>{
+  //   const page = await browser.newPage();
+  //   await page.goto('http://localhost:3000/2');
+  //   await expect(page.url()).toContain('/2')
 
-    await page.waitForSelector('div.logo > span')
-    let element = await page.$('div.logo > span')   
-    let value = await page.evaluate(el => el?.textContent, element)
-    //o teste irá falhar caso o valor seja diferente de lotofácil
-    expect(value).toContain('lotofácil')
-    await page.close();
+  //   await page.waitForSelector('div.logo > span')
+  //   let element = await page.$('div.logo > span')   
+  //   let value = await page.evaluate(el => el?.textContent, element)
+  //   //o teste irá falhar caso o valor seja diferente de lotofácil
+  //   expect(value).toContain('lotofácil')
+  //   await page.close();
 
-  });
-  test('validando a quarta rota, LOTOMANIA.', async () =>{
-    const page = await browser.newPage();
-    await page.goto('http://localhost:3000/3');
-    await expect(page.url()).toContain('/3')
+  // });
+  // test('validando a quarta rota, LOTOMANIA.', async () =>{
+  //   const page = await browser.newPage();
+  //   await page.goto('http://localhost:3000/3');
+  //   await expect(page.url()).toContain('/3')
 
-    await page.waitForSelector('div.logo > span')
-    let element = await page.$('div.logo > span')   
-    let value = await page.evaluate(el => el?.textContent, element)
-    //o teste irá falhar caso o valor seja diferente de lotomania
-    expect(value).toContain('lotomania')
-    await page.close();
-
-
-  });
-  test('validando a quinta rota, TIMEMANIA.', async () =>{
-    const page = await browser.newPage();
-    await page.goto('http://localhost:3000/4');
-    await expect(page.url()).toContain('/4')
-
-    await page.waitForSelector('div.logo > span')
-    let element = await page.$('div.logo > span')   
-    let value = await page.evaluate(el => el?.textContent, element)
-    //o teste irá falhar caso o valor seja diferente de timemania
-    expect(value).toContain('timemania')
-    await page.close();
+  //   await page.waitForSelector('div.logo > span')
+  //   let element = await page.$('div.logo > span')   
+  //   let value = await page.evaluate(el => el?.textContent, element)
+  //   //o teste irá falhar caso o valor seja diferente de lotomania
+  //   expect(value).toContain('lotomania')
+  //   await page.close();
 
 
-  });
-  test('validando a sexta rota, DIA DE SORTE.', async () =>{
-    const page = await browser.newPage();
-    await page.goto('http://localhost:3000/5');
-    await expect(page.url()).toContain('/5')
+  // });
+  // test('validando a quinta rota, TIMEMANIA.', async () =>{
+  //   const page = await browser.newPage();
+  //   await page.goto('http://localhost:3000/4');
+  //   await expect(page.url()).toContain('/4')
 
-    await page.waitForSelector('div.logo > span')
-    let element = await page.$('div.logo > span')   
-    let value = await page.evaluate(el => el?.textContent, element)
-    //o teste irá falhar caso o valor seja diferente de dia de sorte
-    expect(value).toContain('dia de sorte')
-    await page.close();
+  //   await page.waitForSelector('div.logo > span')
+  //   let element = await page.$('div.logo > span')   
+  //   let value = await page.evaluate(el => el?.textContent, element)
+  //   //o teste irá falhar caso o valor seja diferente de timemania
+  //   expect(value).toContain('timemania')
+  //   await page.close();
 
 
-  });
-  
+  // });
+  // test('validando a sexta rota, DIA DE SORTE.', async () =>{
+  //   const page = await browser.newPage();
+  //   await page.goto('http://localhost:3000/5');
+  //   await expect(page.url()).toContain('/5')
+
+  //   await page.waitForSelector('div.logo > span')
+  //   let element = await page.$('div.logo > span')   
+  //   let value = await page.evaluate(el => el?.textContent, element)
+  //   //o teste irá falhar caso o valor seja diferente de dia de sorte
+  //   expect(value).toContain('dia de sorte')
+  //   await page.close();
+
+
+  // });
+
 })
 
 
